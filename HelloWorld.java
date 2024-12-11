58ea741caabbb2e4e3440ba8d9f8b54e150e2765
Abir Allaoui <abirallaoui0@gmail.com>
2024-12-11 08:19:01 +0000

    message dans HelloWorld regler lerreur

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Faites une Modication au niveau de votre classe HelloWorld et dites moi ce qui va se
-passer.");
+        System.out.println("Faites une Modication au niveau de votre classe HelloWorld et dites moi ce qui va se passer.");
     }
 }
